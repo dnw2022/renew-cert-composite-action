ee0c874645338ade54d9c0b5f67adf2ac7a5f738
Jeroen Bijlsma <jeroen_bijlsma@yahoo.com>
2022-04-14 09:05:09 +0200

    Use specified secret names for storing secrets

--- renew_certs.sh
+++ renew_certs.sh
@@ -79,8 +79,8 @@ then
   cat cert.pfx | base64 > cert.base64
 
   echo "Store thumprint and pfx file as github secrets"
-  gh secret set CERT_THUMBPRINT --org $ORGANIZATION --visibility all --body "$FINGER_PRINT" 
-  gh secret set CERT_PFX_BASE64 --org $ORGANIZATION --visibility all < cert.base64
+  gh secret set $CERT_THUMBPRINT_SECRET_NAME --org $ORGANIZATION --visibility all --body "$FINGER_PRINT" 
+  gh secret set $CERT_SECRET_NAME --org $ORGANIZATION --visibility all < cert.base64
 
   echo "Cleanup Files"
   rm ./cloudflare.ini
